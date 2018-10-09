f96e647f928d5caa81818a28a167cb55c3b13d12
amit verma <amit.verma1@aajtak.com>
2018-10-09 23:17:49 +0530

    touch slide

--- main.js
+++ main.js
@@ -4,16 +4,53 @@
    var slide = document.getElementsByClassName("item");
    var outerEl = document.querySelector(".slider-stage");
    var mrgin = 10;
-   var itemShow = 6;
+   var itemShow = 3;
    var itemWidth = slider.clientWidth / itemShow; 
    var outerwidth = itemWidth * slide.length;
+   var isDown = false;
+   var startx;
+   var scrollLeft;
     
-    console.dir(slider.clientWidth);
+    
+    
+    outerEl.addEventListener('mousedown', (e) =>{
+      isDown = true;
+      startx = e.pageX - outerEl.offsetLeft;
+      scrollLeft = outerEl.scrollLeft;   
+    })
+    
+    
+    outerEl.addEventListener('mouseleave', () =>{
+       isDown = false;
+    })
+    
+    
+    outerEl.addEventListener('mouseup', () =>{
+       isDown = false;
+    })
+    
+    
+    outerEl.addEventListener('mousemove', (e) =>{
+        
+      if(!isDown) return; // stop the function from running;
+      e.preventDefault();
+      const x = e.pageX - outerEl.offsetLeft;
+      const dist = (x - startx) * 2;
+      var m = scrollLeft - dist; 
+      outerEl.style.transform = 'translate3d('+ m * counter +'px, 0px, 0px)'; 
+        console.log(m);
+    })  
+    
+    
+    
+    
+    
+    
+    
     outerEl.style.width = outerwidth + "px";
     outerEl.style.transition = "all 0.25s ease 0s";
     
     Array.prototype.forEach.call(slide, item => {
-        console.log(item.style.width);
         item.style.width = itemWidth + 'px';
         item.style.marginRight = mrgin + 'px';        
         for(var idx=0;idx<itemShow;idx++){
@@ -32,9 +69,6 @@
           console.log(counter);
           outerEl.style.transform = 'translate3d('+ itemWidth * counter +'px, 0px, 0px)';
         }
-        Array.prototype.forEach.call(slide, item => {
-            console.log(slide)            ;
-        });
          for(var idx=(counter * -1);idx<(itemShow + (counter * -1));idx++){
             slide[idx].classList.add('active');
         }    
